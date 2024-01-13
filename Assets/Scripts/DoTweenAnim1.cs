bf883d65f1f2f1c6d99c28a549d969b8fcee09a8
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-13 22:39:18 +0900

    ボタン1のアニメーション変更

--- Assets/Scripts/DoTweenAnim1.cs
+++ Assets/Scripts/DoTweenAnim1.cs
@@ -5,7 +5,7 @@ using DG.Tweening;
 
 public class DoTweenAnim1 : TweenBase
 {
-    private Tween _currentTween;
+    private Tween _currentTween = default;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +23,10 @@ public class DoTweenAnim1 : TweenBase
     protected override void UiLoopAnimation()
     {
         _currentTween = transform.DOScale(0.8f, 1)
-                                 .SetLoops(-1,LoopType.Yoyo)
+                                 .SetLoops(-1, LoopType.Yoyo)
                                  .SetEase(Ease.Linear);
 
+        _targetImage.DOFade(0.5f, 1)
+                    .SetLoops(-1, LoopType.Yoyo);
     }
 }
