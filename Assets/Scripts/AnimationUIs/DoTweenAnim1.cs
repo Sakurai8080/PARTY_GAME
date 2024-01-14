0992b4305a532f38fdb7664e475c27ee57fe0cd0
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-14 12:43:22 +0900

    TweenBaseとAnimationUI1のリファクタリングと機能追加

--- Assets/Scripts/AnimationUIs/DoTweenAnim1.cs
+++ Assets/Scripts/AnimationUIs/DoTweenAnim1.cs
@@ -5,42 +5,47 @@ using DG.Tweening;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using System;
+using System.Linq;
 
 public class DoTweenAnim1 : TweenBase
 {
-    private Tween _currentTween = null;
-
     private void OnEnable()
     {
         transform.localScale = Vector3.zero;
         PlayAnimation();
     }
 
-    private void PlayAnimation()
+    private void OnDisable()
     {
-        _currentTween = transform.DOScale(1, 1f)
-                                 .SetEase(Ease.InSine)
-                                 .SetDelay(0.5f)
-                                 .OnComplete(async () =>
-                                 {
-                                     await AnimationDelay(1000);
-                                     UiLoopAnimation();
-                                 });
+        ImageAlphaController(_targetImage, 1);
+        KillTweens();
     }
 
+    protected override void PlayAnimation()
+    {
+        _currentScaleTween= transform.DOScale(1, 1f)
+                                     .SetEase(Ease.InSine)
+                                     .SetDelay(0.5f)
+                                     .OnComplete(async () =>
+                                     {
+                                         await AnimationDelay(1000);
+                                         UiLoopAnimation();
+                                     });
+    }
+
+    protected override void UiLoopAnimation()
+    {
+        _currentScaleTween = transform.DOScale(0.8f, 1)
+                                      .SetLoops(-1, LoopType.Yoyo)
+                                      .SetEase(Ease.Linear);
+
+        _currentFadeTween = _targetImage.DOFade(0.5f, 1)
+                                        .SetLoops(-1, LoopType.Yoyo);
+    }
 
     private async UniTask AnimationDelay(double delayTime)
     {
         await UniTask.Delay(TimeSpan.FromMilliseconds(delayTime));
     }
 
-    protected override void UiLoopAnimation()
-    {
-        _currentTween = transform.DOScale(0.8f, 1)
-                                 .SetLoops(-1, LoopType.Yoyo)
-                                 .SetEase(Ease.Linear);
-
-        _targetImage.DOFade(0.5f, 1)
-                    .SetLoops(-1, LoopType.Yoyo);
-    }
 }
