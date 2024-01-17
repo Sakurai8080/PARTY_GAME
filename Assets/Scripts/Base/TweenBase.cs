0a8dea9411b0b48d34851925e2260a11d86b6401
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-17 17:15:52 +0900

    Baseクラスに選択時の動きを追加

--- Assets/Scripts/Base/TweenBase.cs
+++ Assets/Scripts/Base/TweenBase.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public abstract class TweenBase : MonoBehaviour
 {
@@ -24,12 +25,31 @@ public abstract class TweenBase : MonoBehaviour
     [SerializeField]
     protected Image _targetImage = default;
 
+    [SerializeField]
+    protected Button _tweensButton = default;
+
+    /// <summary>現在起動しているScaleに関わるTween操作用</summary>
     protected Tween _currentScaleTween = null;
+    /// <summary>現在起動しているFadeに関わるTween操作用</summary>
     protected Tween _currentFadeTween = null;
 
     protected abstract void PlayAnimation();
     protected abstract void UiLoopAnimation();
 
+    protected void Start()
+    {
+        _tweensButton.OnClickAsObservable()
+                     .TakeUntilDestroy(this)
+                     .Subscribe(_ =>
+                     {
+                         ImageAlphaController(_targetImage, 1);
+                         KillTweens();
+                         SelectedAnimation();
+                     });
+                     
+    }
+
+    //WARNING:引数はミリ秒
     protected async UniTask AnimationDelay(double delayTime)
     {
         await UniTask.Delay(TimeSpan.FromMilliseconds(delayTime));
@@ -46,5 +66,13 @@ public abstract class TweenBase : MonoBehaviour
     {
         _currentFadeTween?.Kill();
         _currentScaleTween?.Kill();
+    }
+
+    protected void SelectedAnimation()
+    {
+        _currentScaleTween = transform.DOScale(1, 0.25f)
+                                      .SetEase(Ease.OutQuint)
+                                      .SetDelay(0.1f)
+                                      .OnComplete(() => transform.DOPunchRotation(new Vector3(180f, 270, -45), 2f, 5, 1f));
     }
 }
