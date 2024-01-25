6e528ab3f9d68acc68bd9d5c44df4a6995fc8843
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-25 15:11:03 +0900

    アニメーションUI4つ目完成

--- Assets/Scripts/UIAnimations/DoTweenAnim4.cs
+++ Assets/Scripts/UIAnimations/DoTweenAnim4.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using System;
+using System.Linq;
 
-public class DoTweenAnim4 : MonoBehaviour
+public class DoTweenAnim4 : TweenBase
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        
+        transform.localScale = Vector3.zero;
+        ImageAlphaController(_targetImage, 1);
+        PlayAnimation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        KillTweens();
+    }
+
+    protected override void PlayAnimation()
+    {
+        _currentScaleTween = _targetImage.DOFade(0.5f, 1)
+                                         .SetEase(Ease.OutFlash);
+
+        _currentScaleTween = transform.DOScale(1, 1f)
+                                      .SetEase(Ease.OutQuint)
+                                      .SetDelay(0.5f)
+                                      .OnComplete(async () =>
+                                      {
+                                          await AnimationDelay(1000);
+                                          UiLoopAnimation();
+                                      });
+    }
+
+    protected override void UiLoopAnimation()
+    {
+        _currentScaleTween = transform.DOScale(0.3f, 1)
+                                      .SetLoops(-1, LoopType.Yoyo)
+                                      .SetEase(Ease.OutCubic);
+
+        _currentScaleTween = _targetImage.DOFade(1f, 1)
+                                         .SetLoops(-1, LoopType.Yoyo);
     }
 }
