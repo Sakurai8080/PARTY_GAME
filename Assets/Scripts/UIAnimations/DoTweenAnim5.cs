89c3d6a24d17ce48962f447a38d77677ef00c6b8
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-27 22:24:55 +0900

    UIアニメーション５つ目を作成

--- Assets/Scripts/UIAnimations/DoTweenAnim5.cs
+++ Assets/Scripts/UIAnimations/DoTweenAnim5.cs
@@ -9,24 +9,28 @@ using System.Linq;
 
 public class DoTweenAnim5 : TweenBase
 {
-
     private void OnEnable()
     {
-
-    }
-
-    private void OnDisable()
-    {
-
+        transform.localScale = Vector3.zero;
+        PlayAnimation();
     }
 
     protected override void PlayAnimation()
     {
-
+        _currentScaleTween = transform.DOScale(1, 1f)
+                                      .SetEase(Ease.InOutBounce)
+                                      .SetDelay(0.5f)
+                                      .OnComplete(async () =>
+                                      {
+                                          await AnimationDelay(1000);
+                                          UiLoopAnimation();
+                                      });
     }
 
     protected override void UiLoopAnimation()
     {
-
+        _currentScaleTween = transform.DOScale(0.1f, 1)
+                                      .SetLoops(-1, LoopType.Yoyo)
+                                      .SetEase(Ease.InBounce);
     }
 }
