b5e936ad3d89dee391f957faec9b1767bf81a2a7
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-28 20:40:18 +0900

    UIAnimation6つ目作成

--- Assets/Scripts/UIAnimations/DoTweenAnim6.cs
+++ Assets/Scripts/UIAnimations/DoTweenAnim6.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 
-public class DoTweenAnim6 : MonoBehaviour
+namespace TweenGroup
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DoTweenAnim6 : TweenBase
     {
-        
-    }
+        private Vector3 _loopScaleAmount = new Vector3(0.1f, 0.1f, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnEnable()
+        {
+            transform.localScale = Vector3.zero;
+            PlayAnimation();
+        }
+
+        protected override void PlayAnimation()
+        {
+            _currentScaleTween = transform.DOScale(1f, 1f)
+                                          .SetEase(Ease.InExpo)
+                                          .SetDelay(0.5f)
+                                          .OnComplete(async () =>
+                                          {
+                                              await AnimationDelay(1000);
+                                              UiLoopAnimation();
+                                          });
+        }
+
+        protected override void UiLoopAnimation()
+        {
+            _currentScaleTween = transform.DOBlendableScaleBy(_loopScaleAmount,1f)
+                                          .SetEase(Ease.InQuad)
+                                          .SetLoops(-1, LoopType.Yoyo);
+        }
     }
 }
