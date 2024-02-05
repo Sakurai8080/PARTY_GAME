07b489b33d43d3c85398a291f983bd7e544175d8
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-02-05 11:15:14 +0900

    パラメータをスクリタブルオブジェクトからの参照に変更

--- Assets/Scripts/Base/TweenBase.cs
+++ Assets/Scripts/Base/TweenBase.cs
@@ -9,18 +9,8 @@ using UniRx;
 
 public abstract class TweenBase : MonoBehaviour
 {
-    [Header("Variable")]
-    [Tooltip("遅らせる時間")]
     [SerializeField]
-    protected float _delayTime = 2f;
-
-    [Tooltip("所要時間")]
-    [SerializeField]
-    protected float _requiredTime = 1f;
-
-    [Tooltip("ターゲット値")]
-    [SerializeField]
-    protected float _targetAmout = 1f;
+    protected TweenData _tweenData;
 
     [SerializeField]
     protected Image _targetImage = default;
