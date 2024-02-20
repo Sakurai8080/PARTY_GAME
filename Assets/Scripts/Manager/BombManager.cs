6ac70e86d62a66931d923dc5430e2fa6d364f829
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-02-20 11:41:46 +0900

    BombManagerの処理にあったTween操作をクラス分け

--- Assets/Scripts/Manager/BombManager.cs
+++ Assets/Scripts/Manager/BombManager.cs
@@ -13,16 +13,10 @@ using System.Linq;
 /// </summary>
 public static class BombManager
 {
-    /// <summary>Bombを格納する辞書</summary>
+    /// <summary>Cardを格納する</summary>
     public static Dictionary<Image, bool> _allBombUIdic = new Dictionary<Image, bool>();
 
-    public static void AllTweenStop(Color resetColor)
-    {
-        Killweens();
-        ResetTweens(resetColor);
-    }
-
-    public static bool BombInChecher(Image image)
+    public static bool BombInChecker(Image image)
     {
         return _allBombUIdic[image];
     }
@@ -35,32 +29,5 @@ public static class BombManager
         _allBombUIdic[inBombImage] = true;
 
         Debug.Log(inBombImage);
-    }
-
-    public static void Killweens()
-    {
-        foreach (var image in _allBombUIdic)
-        {
-            image.Key.transform.DOKill();
-            image.Key.DOKill();
-        }
-    }
-
-    public static void ResetTweens(Color resetColor)
-    {
-        foreach (var image in _allBombUIdic)
-        {
-            image.Key.transform.DOScale(1, 0.25f)
-                               .SetEase(Ease.InBack);
-            image.Key.transform.DORotate(Vector3.zero, 0.25f)
-                               .SetEase(Ease.InBack);
-            image.Key.DOFade(1, 0.25f);
-            image.Key.DOColor(resetColor, 0.25f);
-        }
-    }
-
-    public static void TweenListClear()
-    {
-        _allBombUIdic.Clear();
     }
 }
