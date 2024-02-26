2cffc746552872a32b035ad95dda536a7d56e500
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-02-26 11:13:51 +0900

    シングルトンを追加

---
+++ Assets/Scripts/DesighPattern/SingletonMonoBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance ==null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    Debug.LogError(typeof(T) + "がシーンに存在しません");
+                }
+            }
+            return instance;
+        }
+    }
+}
