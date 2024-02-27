f5b4629cb3eef18ad2d8777711b15caa0e028abd
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-02-27 15:53:23 +0900

    不要なネームスペースの削除

--- Assets/Scripts/Base/TweenBase.cs
+++ Assets/Scripts/Base/TweenBase.cs
@@ -106,6 +106,8 @@ namespace TweenGroup
                                          });
                 return;
             }
+
+            GameManager.Instance.SceneLoader("MainScene");
         }
 
         protected void RotationReset()
