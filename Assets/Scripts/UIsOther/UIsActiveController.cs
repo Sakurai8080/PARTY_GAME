1d3bc29e30f6ab1a6883bdfeec881bfec62eb3e7
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-02-27 15:47:53 +0900

    TB用のメインUIを表示するクラス追加

---
+++ Assets/Scripts/UIsOther/UIsActiveController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// UIの操作をするクラス
+/// </summary>
+public class UIsActiveController : MonoBehaviour
+{
+    [Header("Variable")]
+    [Tooltip("TestUIをまとめたグループ")]
+    [SerializeField]
+    private CanvasGroup _testUIGroup;
+
+    [Tooltip("TestUIをまとめた親空オブジェクト")]
+    [SerializeField]
+    private GameObject _testUIParent;
+
+    private bool _isUIsActive = false;
+
+    public void ToggleUIsVisibility()
+    {
+        _isUIsActive = !_isUIsActive;
+        _testUIGroup.alpha = Convert.ToInt32(_isUIsActive);
+        _testUIParent.gameObject.SetActive(_isUIsActive);
+    }
+}
