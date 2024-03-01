e104c97dce4083f389ba2b1af9dcd787a71c4c00
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-03-01 11:20:50 +0900

    ルーレットゲームの追加

---
+++ Assets/Scripts/Roulette/UIs/RouletteUIPresenter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using System.Linq;
+using UnityEngine.UI;
+
+
+public class RouletteUIPresenter : PresenterBase
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
