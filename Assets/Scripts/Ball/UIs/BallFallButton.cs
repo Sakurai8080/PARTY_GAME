e3020c18cf735c1cfe475a551349e085da7eb053
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-03-06 16:14:13 +0900

    ボールゲームを開始するためのボールを落とすボタンの追加

---
+++ Assets/Scripts/Ball/UIs/BallFallButton.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using System.Linq;
+using UnityEngine.UI;
+
+public class BallFallButton : MonoBehaviour
+{
+    public IObservable<Unit> FallButtonClickObserver => _fallButtonClickSubject;
+
+    private Subject<Unit> _fallButtonClickSubject = new Subject<Unit>();
+
+    [SerializeField]
+    Button _ballFallButton = default;
+
+    private void Start()
+    {
+        _ballFallButton.OnClickAsObservable()
+                       .TakeUntilDestroy(this)
+                       .Subscribe(_ =>
+                       {
+                           _fallButtonClickSubject.OnNext(Unit.Default);
+                           gameObject.SetActive(false);
+                       });
+    }
+}
