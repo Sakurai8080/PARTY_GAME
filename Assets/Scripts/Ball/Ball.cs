979b37b7a89973442f285d2f412383f6cf075654
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-03-10 22:30:14 +0900

    ボールに機能をもたせるクラスの追加

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Linq;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+
+    private TextMeshProUGUI _nameText = default;
+
+    private void Start()
+    {
+        _nameText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TextDestroy();
+    }
+
+    void TextDestroy()
+    {
+        _nameText.enabled = false;
+    }
+}
