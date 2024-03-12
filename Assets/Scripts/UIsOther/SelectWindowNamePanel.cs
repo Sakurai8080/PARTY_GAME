d56bfea5b41fcca10ce01e2fac8e2d9e88cc9a85
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-03-12 18:22:11 +0900

    ゲームセレクト画面で名前とライフが表示されるように変更

---
+++ Assets/Scripts/UIsOther/SelectWindowNamePanel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Linq;
+using TMPro;
+
+public class SelectWindowNamePanel : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> _nameTmpro = new List<TextMeshProUGUI>();
+
+    char _semicolon = ':';
+
+    private void Awake()
+    {
+        for (int i = 0; i < NameLifeManager.Instance.GamePlayerAmount; i++)
+        {
+            string currentPlayerName = $"{NameLifeManager.Instance.NameList[i]}:";
+            _nameTmpro[i].text = (currentPlayerName.Length >= 4) ? currentPlayerName.Substring(0, 3)+_semicolon : currentPlayerName;
+            _nameTmpro[i].gameObject.SetActive(true);
+        }
+    }
+}
