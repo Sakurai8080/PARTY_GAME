8e7e169690fc58779291a71b59a4b820399b0a4f
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-04-11 13:12:34 +0900

    ボールに与える力の変更

--- Assets/Scripts/BallGame/Managers/BallController.cs
+++ Assets/Scripts/BallGame/Managers/BallController.cs
@@ -106,7 +106,7 @@ public class BallController : SingletonMonoBehaviour<BallController>
     /// <param name="rigidbody">ボール飛ばすため</param>
     private void RandomForce(Rigidbody rigidbody)
     {
-        float randomXforce = UnityEngine.Random.Range(-100f, 100f), randomZforce = UnityEngine.Random.Range(-100f, 100f);
+        float randomXforce = UnityEngine.Random.Range(-20f, 20f), randomZforce = UnityEngine.Random.Range(-20f, 20f);
         rigidbody.AddForce(randomXforce, 0, randomZforce);
     }
 
