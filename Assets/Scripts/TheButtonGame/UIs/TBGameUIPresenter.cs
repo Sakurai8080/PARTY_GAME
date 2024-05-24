e5bd2f6a5aec2f0d0300b67d45c1efc751729f29
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-05-24 10:16:55 +0900

    ボタンゲームの選択指示をする説明TMPの追加

--- Assets/Scripts/TheButtonGame/UIs/TBGameUIPresenter.cs
+++ Assets/Scripts/TheButtonGame/UIs/TBGameUIPresenter.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UniRx;
 using Cysharp.Threading.Tasks;
+using TMPro;
 
 /// <summary>
 /// ボタンゲームのUIプレゼンター
 /// </summary>
 public class TBGameUIPresenter : PresenterBase
 {
+    [Tooltip("選択を指示する説明用TMP")]
+    [SerializeField]
+    private TextMeshProUGUI _naviTMP = default;
+
     [Tooltip("各セレクトボタン")]
     [SerializeField]
     private TBGameSelectBtn[] _button = default;
@@ -22,6 +27,7 @@ public class TBGameUIPresenter : PresenterBase
                       {
                           TBGameManager.Instance.MissButtonChecker(button);
                           _currentOrderUIs.gameObject.SetActive(false);
+                          _naviTMP.gameObject.SetActive(false);
                       });
         }
 
@@ -31,5 +37,9 @@ public class TBGameUIPresenter : PresenterBase
                           TBGameManager.Instance.ButtonRandomHide();
                           FadeManager.Instance.OrderChangeFadeAnimation().Forget();
                       }).AddTo(this);
+
+        FadeManager.Instance.NameAnimCompletedObserver
+                .TakeUntilDestroy(this)
+                .Subscribe(_ => _naviTMP.gameObject.SetActive(true));
     }
 }
